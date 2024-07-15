c3f50bcbae34f7d7ab559889601ae419da8ec231
hackanonimous <gabriel12637>
2024-07-15 10:25:50 -0500

    archivo

--- modulos/homework/main.py
+++ modulos/homework/main.py
@@ -6,13 +6,13 @@ def mayor_edad(edad):
         print("es mator de edad")
     else:
         print("es menor de edad")
-def es_mayor(lista):
-    """funcion para saber el numero mayor de una lista"""
-    mayor=lista[0]
+def es_menor(lista):
+    """funcion para saber el numero menor de una lista"""
+    menor=lista[0]
     for n in lista:
-        if n > mayor:
-            mayor=n
-    return mayor
+        if n >menor:
+            menor=n
+    return menor
 def f_cuenta_vocales(text:str):
     """funcion para contar la cantidad de vocales a que aparecen en
     un texto"""
